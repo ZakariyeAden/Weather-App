2bd33cc32bba1a432f2a867e9c0bdfda5d682902
Zakariye Aden <zakariye.aden00@gmail.com>
2022-02-28 15:03:01 -0600

    adding media queries

--- src/index.js
+++ src/index.js
@@ -1,7 +1,69 @@
 import './style.css';
-let weather = {
-  "apiKey": "d22e0d71488b678eaa14f3c03eeb8428",
-fetchWeather: function(){
-    fetch()
-  },
+const api = {
+  key: "d22e0d71488b678eaa14f3c03eeb8428",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+const search = document.querySelector('.searchBar');
+search.addEventListener('keypress', setQuery);
+
+function setQuery(evt){
+  if(evt.keyCode == 13){
+    getResults(search.value);
+  }
+}
+
+function getResults(query){
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  .then(weather => {
+    return weather.json();
+  }).then(displayResults);
+}
+function displayResults(weather){
+  console.log(weather);
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+  let weatherEL = document.querySelector('.current .weather');
+  weatherEL.innerText = weather.weather[0].main;
+
+  let hiLow = document.querySelector('.hi-low');
+  hiLow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+}
+
+function dateBuilder(d){
+  let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"];
+  let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday", 
+  "Thursday",
+  "Friday", 
+  "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
 }
